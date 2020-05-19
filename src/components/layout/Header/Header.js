47fdf7b2ee8aa8b42c7ebe11f623367c5e92b4c1
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-05-19 20:18:34 +0200

    hide my ads and/logout if the user is not logged in

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getUser } from '../../../redux/userRedux.js';
 
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -12,44 +14,51 @@ import clsx from 'clsx';
 
 import styles from './Header.module.scss';
 
-const Component = ({ className }) => (
+const Component = ({ className, user }) => (
   <div className={clsx(className, styles.root)}>
     <Navbar bg="dark" variant="dark" sticky="top">
       <Navbar.Brand href="#home">Bulletin Board</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="https://google.com">
-          <i className="fas fa-user-alt"></i>{' '}
+      { user.authenticated ? (
+        <Nav className="mr-auto">
+          <Nav.Link href="/">
+            <i className="far fa-address-card"></i>{' '}
+            My ads
+          </Nav.Link>
+          <Nav.Link href="/">
+            <i className="fas fa-sign-out-alt"></i>{' '}
+            Logout
+          </Nav.Link>
+        </Nav>
+      ) : (
+
+        <Nav className="mr-auto">
+          <Nav.Link href="https://google.com">
+            <i className="fas fa-user-alt"></i>{' '}
           Login with Google
-        </Nav.Link>
-        <Nav.Link href="/">
-          <i className="far fa-address-card"></i>{' '}
-          My ads
-        </Nav.Link>
-        <Nav.Link href="/">
-          <i className="fas fa-sign-out-alt"></i>{' '}
-          Logout
-        </Nav.Link>
-      </Nav>
+          </Nav.Link>
+        </Nav>
+      )}
     </Navbar>
   </div >
 );
 
 Component.propTypes = {
   className: PropTypes.string,
+  user: PropTypes.object,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  user: getUser(state),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as Header,
-  // Container as Header,
+  //Component as Header,
+  Container as Header,
   Component as HeaderComponent,
 };
