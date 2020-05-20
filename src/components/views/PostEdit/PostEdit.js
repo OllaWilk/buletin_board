bcde28ccc5f0de7753fcddf8fb323215cf5ff7ba
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-05-20 16:57:19 +0200

    add post Edit view

--- src/components/views/PostEdit/PostEdit.js
+++ src/components/views/PostEdit/PostEdit.js
@@ -3,35 +3,101 @@ import PropTypes from 'prop-types';
 
 import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { connect } from 'react-redux';
+import { getPostById } from '../../../redux/postsRedux';
+import { getUser } from '../../../redux/userRedux';
 
 import styles from './PostEdit.module.scss';
 
-const Component = ({className, children}) => (
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+
+const Component = ({className, post, user}) => (
   <div className={clsx(className, styles.root)}>
     <h2>PostEdit</h2>
-    {children}
+
+    <Form>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridTitle" >
+          <Form.Label>Title</Form.Label>
+          <Form.Control defaultValue={post.title} required type="text"/>
+        </Form.Group>
+      </Form.Row>
+      <Form.Group controlId="postContent">
+        <Form.Label>Description</Form.Label>
+        <Form.Control required as="textarea" rows="3" defaultValue={post.description} />
+      </Form.Group>
+
+      <Form.Row >
+        <Form.Group as={Col} sm={12} md={4} controlId="formGridPrice">
+          <Form.Label>Price</Form.Label>
+          <Form.Control type="text" defaultValue={post.price} />
+        </Form.Group>
+        <Form.Group as={Col} sm={12} md={4} controlId="formGridLocation">
+          <Form.Label>Location</Form.Label>
+          <Form.Control required type="text" defaultValue={post.location} />
+        </Form.Group>
+        <Form.Group as={Col} sm={12} md={4}scontrolId="formGridEmail">
+          <Form.Label>E-mail</Form.Label>
+          <Form.Control required type="email" defaultValue={post.mail} />
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} sm={12} md={4} controlId="postForm">
+          <Form.Label>What is state of item you are selling</Form.Label>
+          <Form.Control as="select"  >
+            <option>{post.sellingState}</option>
+            <option>new</option>
+            <option>used</option>
+            <option>broken</option>
+            <option>in parts</option>
+            <option>other</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group as={Col} controlId="postDelivery">
+          <Form.Label>Shipping</Form.Label>
+          <Form.Control as="select">
+            <option >{post.shipping}</option>
+            <option>Only pickup</option>
+            <option>Delivery</option>
+          </Form.Control>
+        </Form.Group>
+      </Form.Row>
+      <Form.Group id="formGridCheckbox">
+        <Form.File
+          id="custom-file"
+          label="add youre photo"
+          custom
+        />
+      </Form.Group>
+      <Button type="submit" variant="success" >Update post</Button>
+      <Button color="secondary" href="/" variant="contained" >Return</Button>
+    </Form>
   </div>
 );
 
 Component.propTypes = {
-  children: PropTypes.node,
+  post: PropTypes.object,
   className: PropTypes.string,
+  user: PropTypes.object,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = (state, props) => ({
+  post: getPostById(state, props.match.params.id),
+  user: getUser(state),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as PostEdit,
-  // Container as PostEdit,
+  // Component as PostEdit,
+  Container as PostEdit,
   Component as PostEditComponent,
+
 };
--- src/components/views/PostEdit/PostEdit.test.js
+++ src/components/views/PostEdit/PostEdit.test.js
@@ -2,9 +2,27 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { PostEditComponent } from './PostEdit';
 
+const mockProps = {
+  post: {
+    id: '1',
+    title: 'disposable Face Masks - Packs',
+    description: 'Lorem ipsum. Lorem lorem.',
+    date: '11.05.2020',
+    mail: 'medical@example.com',
+    sellingState: 'New',
+    location: 'London',
+    shipping: 'After payment',
+    image: 'https://images.pexels.com/photos/3786126/pexels-photo-3786126.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+    price: 0.99,
+  },
+  user: {
+    authenticated: true,
+  },
+};
+
 describe('Component PostEdit', () => {
   it('should render without crashing', () => {
-    const component = shallow(<PostEditComponent />);
+    const component = shallow(<PostEditComponent {...mockProps} />);
     expect(component).toBeTruthy();
   });
 });
