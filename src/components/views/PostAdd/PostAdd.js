d8a33681697499d471300d2172f24d79737e9116
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-05-20 17:33:26 +0200

    change postAdd from class component into function component. initial  action to add post

--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import styles from './PostAdd.module.scss';
 import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+//import { connect } from 'react-redux';
+//import { addPost } from '../../../redux/postsRedux';
 
 import Container from '@material-ui/core/Container';
 
@@ -12,81 +12,99 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 
-const Component = ({ className }) => {
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    console.log('Submitted');
-  };
+class Component extends React.Component {
 
-  return (
-    <div className={clsx(className, styles.root)}>
-      <Container  maxWidth="lg">
-        <Form autoComplete="off" onSubmit={e => handleSubmit(e)}>
+  state = {
+    postData: {
+      id: '',
+      title: '',
+      description: '',
+      date: '',
+      updateDate: '',
+      mail: '',
+      sellingState: '',
+      location: '',
+      shipping: '',
+      image: '',
+      price: '',
+    },
+  }
 
-          <Form.Row>
-            <Form.Group as={Col} controlId="formGridTitle" >
-              <Form.Label>Title</Form.Label>
-              <Form.Control type="text" placeholder="use catching words"  required />
-            </Form.Group>
-          </Form.Row>
-          <Form.Group controlId="postContent">
-            <Form.Label>Description</Form.Label>
-            <Form.Control required as="textarea" rows="3" placeholder="Describe the object or matter of your post" />
-          </Form.Group>
+  static propTypes = {
+    className: PropTypes.string,
+    addPost: PropTypes.func,
+  }
 
-          <Form.Row >
-            <Form.Group as={Col} sm={12} md={4} controlId="formGridPrice">
-              <Form.Label>Price</Form.Label>
-              <Form.Control type="text" placeholder="Type price, for free item type 0" />
-            </Form.Group>
-            <Form.Group as={Col} sm={12} md={4} controlId="formGridLocation">
-              <Form.Label>Location</Form.Label>
-              <Form.Control required type="text" placeholder="Enter your location"/>
-            </Form.Group>
-            <Form.Group as={Col} sm={12} md={4}scontrolId="formGridEmail">
-              <Form.Label>E-mail</Form.Label>
-              <Form.Control required type="email" placeholder="name@example.com" />
-            </Form.Group>
-          </Form.Row>
+  render() {
+    const { className } = this.props;
+    return (
+      <div className={clsx(className, styles.root)}>
+        <Container  maxWidth="lg">
+          <Form autoComplete="off">
 
-          <Form.Row>
-            <Form.Group as={Col} sm={12} md={4} controlId="postForm">
-              <Form.Label>What is state of item you are selling</Form.Label>
-              <Form.Control as="select">
-                <option>new</option>
-                <option>used</option>
-                <option>broken</option>
-                <option>in parts</option>
-                <option>other</option>
-              </Form.Control>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridTitle" >
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="text" placeholder="use catching words"  required />
+              </Form.Group>
+            </Form.Row>
+            <Form.Group controlId="postContent">
+              <Form.Label>Description</Form.Label>
+              <Form.Control required as="textarea" rows="3" placeholder="Describe the object or matter of your post" />
             </Form.Group>
-            <Form.Group as={Col} controlId="postDelivery">
-              <Form.Label>Shipping</Form.Label>
-              <Form.Control as="select">
-                <option>Only pickup</option>
-                <option>Delivery</option>
-              </Form.Control>
-            </Form.Group>
-          </Form.Row>
-          <Form.Group id="formGridCheckbox">
-            <Form.File
-              id="custom-file"
-              label="add youre photo"
-              custom
-            />
-          </Form.Group>
-          <Button type="submit" variant="success" >Save</Button>
-          <Button color="secondary" href="/" variant="contained" >Return</Button>
-        </Form>
-      </Container>
-    </div>
-  );
-};
 
-Component.propTypes = {
-  className: PropTypes.string,
-};
+            <Form.Row >
+              <Form.Group as={Col} sm={12} md={4} controlId="formGridPrice">
+                <Form.Label>Price</Form.Label>
+                <Form.Control type="text" placeholder="Type price, for free item type 0" />
+              </Form.Group>
+              <Form.Group as={Col} sm={12} md={4} controlId="formGridLocation">
+                <Form.Label>Location</Form.Label>
+                <Form.Control required type="text" placeholder="Enter your location"/>
+              </Form.Group>
+              <Form.Group as={Col} sm={12} md={4}scontrolId="formGridEmail">
+                <Form.Label>E-mail</Form.Label>
+                <Form.Control required type="email" placeholder="name@example.com" />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+              <Form.Group as={Col} sm={12} md={4} controlId="postForm">
+                <Form.Label>What is state of item you are selling</Form.Label>
+                <Form.Control as="select">
+                  <option>new</option>
+                  <option>used</option>
+                  <option>broken</option>
+                  <option>in parts</option>
+                  <option>other</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group as={Col} controlId="postDelivery">
+                <Form.Label>Shipping</Form.Label>
+                <Form.Control as="select">
+                  <option>Only pickup</option>
+                  <option>Delivery</option>
+                </Form.Control>
+              </Form.Group>
+            </Form.Row>
+            <Form.Group id="formGridCheckbox">
+              <Form.File
+                id="custom-file"
+                label="add youre photo"
+                custom
+              />
+            </Form.Group>
+            <Button type="submit" variant="success" >Save</Button>
+            <Button color="secondary" href="/" variant="contained" >Return</Button>
+          </Form>
+        </Container>
+      </div>
+    );
+  }
+
+}
+
 
 // const mapStateToProps = state => ({
 //   someProp: reduxSelector(state),
